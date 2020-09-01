e552d51d8d5dfa950ef7b1b93ae60cff25ce395e
liangxibing <liangxibing@gmail.com>
2020-09-01 10:07:50 +0800

    修改登陆背景图文件夹名称位loginBg，能让文件夹在Linux环境下被访问到

--- system/inc/manageui.inc.php
+++ system/inc/manageui.inc.php
@@ -1,6 +1,6 @@
 <?php	if(!defined('IN_PHPMYWIND')) exit('Request Error!');
 
-$cfg_loginbgimg = 'templates/images/loginbg/loginBg_09.jpg';
+$cfg_loginbgimg = 'templates/images/loginBg/loginBg_09.jpg';
 $cfg_loginbgcolor = '';
 $cfg_loginbgrepeat = '1';
 $cfg_loginbgpos = '1';
